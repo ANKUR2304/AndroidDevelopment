696635dcb63c5545b01acca5c2229ecdde792f10
ANKUR2304 <jakharankur5@gmail.com>
2024-06-16 01:47:11 +0530

    Added Toast to Button click

--- 1. Greetings App/app/src/main/java/com/example/greetingsapp/MainActivity.java
+++ 1. Greetings App/app/src/main/java/com/example/greetingsapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.greetingsapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +14,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button sayHelloBtn;
+    private EditText nameInputET;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        nameInputET = findViewById(R.id.inputNameET);
+        sayHelloBtn = findViewById(R.id.sayHelloBtn);
+        sayHelloBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputName = nameInputET.getText().toString();
+                String toastMessage = "Welcome " + inputName + " to our app!";
+                Toast.makeText(
+                        MainActivity.this,
+                        toastMessage,
+                        Toast.LENGTH_LONG
+                        ).show();
+            }
+        });
     }
 }
