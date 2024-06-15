1b8ad850c76700625f81765f6b3e512f448f3169
ANKUR2304 <jakharankur5@gmail.com>
2024-06-16 00:47:32 +0530

    Basic UI

--- 1. Greetings App/app/src/main/java/com/example/greetingsapp/MainActivity.java
+++ 1. Greetings App/app/src/main/java/com/example/greetingsapp/MainActivity.java
@@ -1,32 +1,19 @@
 package com.example.greetingsapp;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        EditText nameInputET = findViewById(R.id.nameInput);
-        Button sayHelloBtn = findViewById(R.id.sayHelloBtn);
-
-        String nameInputString = nameInputET.getText().toString();
-        sayHelloBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(
-                        MainActivity.this,
-                        "Welcome " + nameInputString + " to our app!",
-                        Toast.LENGTH_LONG
-                ).show();
-            }
-        });
     }
 }
