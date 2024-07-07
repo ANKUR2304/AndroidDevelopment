cd339552921f850ec7d4824ceb4bbccd5fbadd78
ANKUR2304 <jakharankur5@gmail.com>
2024-07-07 17:16:28 +0530

    Added - chooser

--- 4. WorkingWithIntents/app/src/main/java/com/example/workingwithintents/WishLuckActivity.java
+++ 4. WorkingWithIntents/app/src/main/java/com/example/workingwithintents/WishLuckActivity.java
@@ -2,6 +2,8 @@ package com.example.workingwithintents;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,8 +15,10 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.Random;
 
 public class WishLuckActivity extends AppCompatActivity {
+    private int luckyNumber;
     private TextView wishTV1;
     private TextView wishTV2;
+    private Button shareBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,15 +27,24 @@ public class WishLuckActivity extends AppCompatActivity {
 
         wishTV1 = findViewById(R.id.wishTV1);
         wishTV2 = findViewById(R.id.wishTV2);
+        shareBtn = findViewById(R.id.shareBtn);
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("EXTRA_MESSAGE");
         wishLuck(name);
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareMessage(name);
+            }
+        });
     }
 
     private void wishLuck(String name){
         String message1 = "Hi " + name + "!";
-        String message2 = "your lucky number is " + generateRandomNumber();
+        luckyNumber = generateRandomNumber();
+        String message2 = "your lucky number is " + luckyNumber;
         wishTV1.setText(message1);
         wishTV2.setText(message2);
     }
@@ -40,5 +53,18 @@ public class WishLuckActivity extends AppCompatActivity {
         Random rand = new Random();
         int randNumber = rand.nextInt(100);
         return randNumber;
+    }
+
+    private void shareMessage(String name){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_SUBJECT,name + " got lucky today!");
+        intent.putExtra(Intent.EXTRA_TEXT, "His lucky number is " + luckyNumber);
+        intent.setType("text/plain"); // It is very important to set type while working with Chooser
+
+        Intent chooser = Intent.createChooser(intent, "Share via :");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(chooser);
+        }
     }
 }
